02a20ef4a105bdec56e2a2552f326e7614dddab0
Geoffrey Royer <geoffrey.royer@gmail.com>
2020-10-25 18:13:08 +0100

    Initial commit

---
+++ src/main/kotlin/dev/ge0/dexter/main.kt
@@ -0,0 +1,6 @@
+package dev.ge0.dexter
+
+fun main(args: Array<String>) {
+    println("Hello world!")
+}
+
---
+++ src/test/kotlin/dev/ge0/dexter/MainTest.kt
@@ -0,0 +1,11 @@
+package dev.ge0.dexter
+
+import org.junit.Test
+import kotlin.test.assertTrue
+
+class MainTest {
+    @Test fun someTest() {
+        assertTrue(true)
+    }
+}
+
